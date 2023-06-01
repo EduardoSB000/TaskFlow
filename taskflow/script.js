ef1f16a8a355e870a7b5a21bedd34efe595c50c3
EduardoSB <121682485+LinuxMystery00@users.noreply.github.com>
2023-06-01 13:57:28 -0300

    Update script.js

--- taskflow/script.js
+++ taskflow/script.js
@@ -1,1 +1,17 @@
-console.log("TaskFlow")
+console.log("TaskFlow");
+
+function Enviar() {
+  const List = document.getElementById('Ul');
+  const newContainer = document.createElement('div');
+  const newTask = document.createElement('h6');
+  const newRadio = document.createElement('input');
+  const Task = document.getElementById('Task').value;
+  
+  newRadio.type = 'radio';
+  newTask.innerText = Task;
+  newTask.style.fontFamily = 'Arial';
+  
+  newContainer.appendChild(newRadio);
+  newContainer.appendChild(newTask);
+  List.appendChild(newContainer);
+}
